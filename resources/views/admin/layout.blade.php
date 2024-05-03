cd731902f14d9f7180920b96ed56e7cf0ff002f6
alok <alokn235@gmail.com>
2024-05-03 11:44:30 +0530

    delete team a tag

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -67,8 +67,6 @@
                             <li class="list-group-item"><a href="auth-normal-sign-in.html"><i
                                         class="feather icon-log-out m-r-5"></i>Logout</a></li>
                         </ul>
-
-                        alok
                     </div>
                 </div>
 
